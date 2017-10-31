fcf276f0a43c271f9d810d859d7244c1d585c150
Yanda Huang <ward11sdo@qq.com>
2017-10-31 15:44:18 -0400

    Added heatbeat, modify baudrate.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -8,23 +8,28 @@ ros::NodeHandle nh;
 Timer t;
 
 const int delay_time = 1000 / ENCODER_FREQ;
+#define HEARTBEAT_CYCLE 500
 
 #define ENCODER_PIN_A 2
 #define ENCODER_PIN_B 3
+#define LED_PIN 13
 
 EncoderReader myEncoderReader(ENCODER_PIN_A, ENCODER_PIN_B);
-IMUReader myIMUReader;
+// IMUReader myIMUReader;
 
+void heartbeat();
 void readAndPublishVelocityHeading();
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(57600);
+  pinMode(LED_PIN, OUTPUT);
   // myIMUReader.realInit();
   nh.initNode();
   nh.advertise(myEncoderReader.get_publisher());
   // nh.advertise(myIMUReader.get_publisher());
   t.every(delay_time, readAndPublishVelocityHeading);
+  t.every(HEARTBEAT_CYCLE, heartbeat);
 }
 
 void loop()
@@ -39,4 +44,11 @@ void readAndPublishVelocityHeading()
   myEncoderReader.publish(nh);
   // myIMUReader.publish(nh);
   nh.spinOnce();
+}
+
+void heartbeat(){
+  static int status = HIGH;
+  digitalWrite(LED_PIN, status);
+  Serial.println(status);
+  status = (status== HIGH)? LOW:HIGH;
 }
