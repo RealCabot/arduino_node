a02118d38865736ffc5c943f85d44d25750ed72f
Andrew <awong2@andrew.cmu.edu>
2017-11-09 19:11:17 -0500

    Motor & PID
    * Fix not building bug
    
    * PID velocity control
    
    Started velocity control on Yanda's original encoder and IMU code in arduino_node and arduino_msg. So far, I am able to move the right wheel, but I was having trouble printing to the screen the values, so I do not know the PID accuracy yet.
    
    * small changes to main with pins and encoderReader.h with ticks per meter
    
    * New PID function in main.cpp
    
    * Damn -Kendrick Lamar
    
    * PID works??
    
    * Made PID class
    
    * Made PID class
    
    * Back to timer with Mega
    
    * Add readme back
    
    * Refactor encoder & PID to motor class
    
    * Publish lspeed and rspeed on /encoder
    
    * Add dynamic speed and PID setting
    
    * Changed PID parameters and loop time. PID works for real(?)
    
    * Swapped motor pins
    
    * Changed EncoderReader.h TICKS_PER_METER
    
    * Motors stop after not receiving commands for a while
    
    * Changed method of reversing motor direction

---
+++ firmware/include/PID.h
@@ -0,0 +1,21 @@
+//
+// Created by andrew on 11/7/17.
+//
+
+#ifndef ARDUINO_NODE_PID_H
+#define ARDUINO_NODE_PID_H
+
+#define PID_FREQ 100
+
+class PID {
+    int Kp, Ki, Kd;
+    float integral = 0;
+    float lastError = 0;
+public:
+    PID(int Kp, int Ki, int Kd);
+    int getPWM(float desiredSpeed, float currSpeed);
+    void setParam(int Kp, int Ki, int Kd);
+};
+
+
+#endif //ARDUINO_NODE_PID_H
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,53 +1,147 @@
 #include <ros.h>
 #include <Arduino.h>
 #include <Timer.h>
-#include "EncoderReader.h"
 #include "IMUReader.h"
+#include "Motor.h"
+#include <arduino_msg/Motor.h>
+#include <geometry_msgs/Vector3.h>
 
 ros::NodeHandle nh;
 Timer t;
 
-const int delay_time = 1000 / ENCODER_FREQ;
+const int PID_DELAY = 1000 / PID_FREQ;
+const int SENSOR_DELAY = 1000 / ENCODER_FREQ;
+const int MOTOR_DELAY = 200; //used in motor timeout
+
+#define MAX_SPEED 0.67		//maximum motorspeed
+#define MOTOR_TIMEOUT 5000  //milliseconds
+
 #define HEARTBEAT_CYCLE 500
 
-#define ENCODER_PIN_A 2
-#define ENCODER_PIN_B 3
+#define ENCODER_RIGHT_PIN_A 18
+#define ENCODER_RIGHT_PIN_B 19
+#define ENCODER_LEFT_PIN_A  2
+#define ENCODER_LEFT_PIN_B  3
+#define MOTOR_RIGHT_PIN_A  6
+#define MOTOR_RIGHT_PIN_B  5
+#define MOTOR_LEFT_PIN_A   10
+#define MOTOR_LEFT_PIN_B   11
 #define LED_PIN 13
 
-EncoderReader myEncoderReader(ENCODER_PIN_A, ENCODER_PIN_B);
-// IMUReader myIMUReader;
+float speed_req_R = 0;
+float speed_req_L = 0;
+long motorUpdateTime = 0;
+//PID coefficients
+const int RKp=35, RKi=5, RKd=15, LKp=35, LKi=5, LKd=15;
 
+Motor motor_L(
+  MOTOR_LEFT_PIN_A, MOTOR_LEFT_PIN_B,
+  ENCODER_LEFT_PIN_A, ENCODER_LEFT_PIN_B,
+  LKp, LKi, LKd
+);
+Motor motor_R(
+  MOTOR_RIGHT_PIN_A, MOTOR_RIGHT_PIN_B,
+  ENCODER_RIGHT_PIN_A, ENCODER_RIGHT_PIN_B,
+  RKp, RKi, RKd
+);
+
+IMUReader myIMUReader;
 void heartbeat();
-void readAndPublishVelocityHeading();
+void updateSensors();
+void motorControl();
+void encoders_publish();
+void setMotorSpeed(const arduino_msg::Motor& speed_msg);
+void setPIDParam(const geometry_msgs::Vector3& pid_param_msg);
+void motorTimeout();
+
+arduino_msg::Motor speed_msg;
+ros::Publisher encoder_publisher("encoder", &speed_msg);
+ros::Subscriber<arduino_msg::Motor> motor_speed_sub("motorSpeed", &setMotorSpeed);
+ros::Subscriber<geometry_msgs::Vector3> pid_param_sub("tunePID", &setPIDParam);
+
 
 void setup()
 {
   Serial.begin(57600);
-  pinMode(LED_PIN, OUTPUT);
+  
   // myIMUReader.realInit();
+
   nh.initNode();
-  nh.advertise(myEncoderReader.get_publisher());
+
+  nh.advertise(encoder_publisher);
   // nh.advertise(myIMUReader.get_publisher());
-  t.every(delay_time, readAndPublishVelocityHeading);
+  nh.subscribe(motor_speed_sub);
+  nh.subscribe(pid_param_sub);
+  //motor settings
+  pinMode(MOTOR_RIGHT_PIN_A, OUTPUT);
+  pinMode(MOTOR_RIGHT_PIN_B, OUTPUT);
+  pinMode(MOTOR_LEFT_PIN_A, OUTPUT);
+  pinMode(MOTOR_LEFT_PIN_B, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  t.every(SENSOR_DELAY, updateSensors);
   t.every(HEARTBEAT_CYCLE, heartbeat);
+  t.every(PID_DELAY, motorControl);
+  t.every(MOTOR_DELAY, motorTimeout);
+
 }
 
 void loop()
 {
+  nh.spinOnce();  
   t.update();
 }
 
-void readAndPublishVelocityHeading()
+void motorControl(){
+  motor_L.go(speed_req_L);
+  motor_R.go(speed_req_R);
+}
+
+void updateSensors()
 {
-  myEncoderReader.update();
+  motor_L.encoder.update();
+  motor_R.encoder.update();
   // myIMUReader.update();
-  myEncoderReader.publish(nh);
+  encoders_publish();
   // myIMUReader.publish(nh);
   nh.spinOnce();
 }
 
+ // blink LED periodically to indicate everything's gonna be alright
 void heartbeat(){
   static int status = HIGH;
   digitalWrite(LED_PIN, status);
   status = (status== HIGH)? LOW:HIGH;
+}
+
+ // publishes the left and right encoder speeds
+void encoders_publish(){ 
+  speed_msg.left_speed = motor_L.encoder.speed;
+  speed_msg.right_speed = motor_R.encoder.speed;
+  speed_msg.header.stamp = nh.now();
+  encoder_publisher.publish( &speed_msg );
+}
+
+ // this callback sets the speed of the left and right motors
+ // subscribes to rostopic "motorSpeed"
+void setMotorSpeed(const arduino_msg::Motor& speed_msg){
+	//constrain motor speeds
+  speed_req_L = constrain(speed_msg.left_speed, -MAX_SPEED, MAX_SPEED);
+  speed_req_R = constrain(speed_msg.right_speed, -MAX_SPEED, MAX_SPEED);
+  motorUpdateTime = millis();  //record last time motors received speeds
+}
+
+ // stop motors if it has been a while since receiving a motor command
+void motorTimeout(){
+  if ((millis() - motorUpdateTime) > MOTOR_TIMEOUT){
+    speed_req_R = 0;
+    speed_req_L = 0;
+  }
+}
+
+ // this callback sets PID coefficient. ONLY USED FOR TUNING PARAMETERS
+void setPIDParam(const geometry_msgs::Vector3& pid_param_msg){
+  // X = proportional term, Y = integral term, Z = derivative term
+  motor_L.pid.setParam(pid_param_msg.x, pid_param_msg.y, pid_param_msg.z);
+  motor_R.pid.setParam(pid_param_msg.x, pid_param_msg.y, pid_param_msg.z);
 }
